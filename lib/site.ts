5159081c5feab32e14342c0223cfdef8b066de5e
Zino Akpoghelie <zinoakpoghelie17@gmail.com>
2026-09-14 07:22:43 +0100

    update

--- lib/site.ts
+++ lib/site.ts
@@ -2,7 +2,7 @@ export const SHOP_NAME = "Summit Auto";
 export const SHOP_PHONE_DISPLAY = "+234 000 000 0000";
 export const SHOP_PHONE_TEL = "+2340000000000";
 export const SHOP_EMAIL = "hello@summitauto.com";
-export const SHOP_LOCATION = "Your city";
+export const SHOP_LOCATION = "Denver, Colorado, USA";
 
 export const NAV_LINKS = [
   { label: "Home", href: "/" },
